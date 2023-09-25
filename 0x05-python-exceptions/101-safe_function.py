55646bbdfc4948f74f29c2e75d95c1b8cabdb472
root <abkarikari23@yahoo.co.uk>
2023-09-25 12:09:02 +0000

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 101-safe_function.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 import sys
 
@@ -22,3 +20,4 @@ def safe_function(fct, *args):
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+
