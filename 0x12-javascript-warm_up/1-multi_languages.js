764e81cdd504b795d531de8451e0b3b559bed5c3
KofiGhana <abkarikari23@yahoo.co.uk>
2023-12-12 07:05:52 +0000

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
