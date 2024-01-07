2cc9830eab8f78705ba1ac614fa696eadf0c894f
KofiGhana <abkarikari23@yahoo.co.uk>
2024-01-07 10:44:18 -0800

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
+""" Python script that fetches https://alx-intranet.hbtn.io/status
+with urllib """
+
 import urllib.request
 
+url = "https://alx-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+if __name__ == '__main__':
+
+    with urllib.request.urlopen(url) as answer:
+        output = answer.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(output)))
+        print("\t- content: {}".format(output))
+        print("\t- utf8 content: {}".format(output.decode('utf-8')))
